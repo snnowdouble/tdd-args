cdb102dddde03dbd83896293aff7b25f56b0aecb
tianfengyuan <tianfengyuan@bytedance.com>
2022-07-10 11:41:12 +0800

    feat:单值string类型解析返回错误绿
    Change-Id: I6591437c9911df1bc8ae64994fcca97db6f23334

--- string_parser.go
+++ string_parser.go
@@ -1,11 +1,19 @@
 package tdd_args
 
+import (
+	"fmt"
+	"strings"
+)
+
 type StringParser struct {
 }
 
 func (s *StringParser) parser(parser *ArgsParser, argsList []string, idx int) error {
 	if len(argsList) > idx+1 {
 		parser.Directory = argsList[idx+1]
+	}
+	if len(argsList) > idx+2 && !strings.Contains(argsList[idx+2], "-") {
+		return fmt.Errorf("too many args error")
 	}
 
 	return nil
