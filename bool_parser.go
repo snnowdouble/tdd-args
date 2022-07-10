7c7925341956e512503df4db1be993bdcbe74a93
tianfengyuan <tianfengyuan@bytedance.com>
2022-07-10 11:03:23 +0800

    feat:单值bool类型解析返回错误绿
    Change-Id: Id0599b1d59248197ee628d91329e69ac84fac2aa

--- bool_parser.go
+++ bool_parser.go
@@ -1,10 +1,18 @@
 package tdd_args
 
+import (
+	"fmt"
+	"strings"
+)
+
 type BoolParser struct {
 }
 
 func (b *BoolParser) parser(parser *ArgsParser, value string) error {
 	parser.Logging = true
+	if !strings.Contains(value, "-") {
+		return fmt.Errorf("too many args error")
+	}
 
 	return nil
 }
